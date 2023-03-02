b6c4997e8d91971e99bd2827a9e612bcfa4292a5
IngDavidTM <david-1011@hotmail.com>
2023-03-02 18:40:40 -0500

    Add redux to login

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import usersReducer from './reducers/createUsers';
+import loginReducer from './reducers/loginUsers';
 
 const store = configureStore({
   reducer: {
     usersCreate: usersReducer,
+    loginUsers: loginReducer,
   },
 });
 
---
+++ src/redux/reducers/loginUsers.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL = 'http://localhost:3000/login';
+const POST_LOGIN = 'wheels_and_deals/login/POST_LOGIN';
+
+const initailState = {
+  token: null,
+};
+
+const loginReducer = (state = initailState, action) => {
+  switch (action.type) {
+    case `${POST_LOGIN}/fulfilled`:
+      return {
+        ...state,
+        token: action.payload.token,
+      };
+    case `${POST_LOGIN}/rejected`:
+      return {
+        ...state,
+        token: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const postLogin = createAsyncThunk(POST_LOGIN, async (user) => {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  return response.json();
+});
+
+export default loginReducer;
