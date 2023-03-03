0a8b45096362ffdd2b57b8b1b122893bb2b68063
IngDavidTM <david-1011@hotmail.com>
2023-03-03 10:48:27 -0500

    Copy information

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Registration/Login';
 import SignUp from './components/Registration/SignUp';
+import ReservedCars from './components/Reservation/reservedCars';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/reserved" element={<ReservedCars />} />
       </Routes>
     </div>
   );
